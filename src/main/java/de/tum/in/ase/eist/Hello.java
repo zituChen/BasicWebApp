aa17bd1cd02c33dac11f9da73635072e19fc34d4
Ben <ben.sheng.de@gmail.com>
2023-10-05 18:43:28 +0200

    updated

--- src/main/java/de/tum/in/ase/eist/Hello.java
+++ src/main/java/de/tum/in/ase/eist/Hello.java
@@ -3,7 +3,7 @@ package de.tum.in.ase.eist;
 public class Hello {
     public static void main(String[] args) {
 
-        System.out.println("Hello 2000");
+        System.out.println("Hello 2000.");
 
 
     }
